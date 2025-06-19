c0c970b9cbdd07224079252d8024459319eb6291
japalominoc <japalominoc@unjbg.edu.pe>
2025-06-18 21:25:33 -0500

    actualizando ejemplo 1: Lectura de los datos del registro

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -1,22 +1,39 @@
 #include<iostream>
 #include<string>
-using  namespace std;
-struct EMP{
-    int num;
-    string nom;
-    float ven[12],sal;
+using namespace std;
+
+struct EMP {
+	int num;
+	string nom;
+	float ven[12], sal;
 };
 
-int main(){
-    EMP EMPLEADOS[100];
-    int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>EMPLEADOS[i].num;
-        getline(cin,EMPLEADOS[i].nom);
-        for(int j=0;j<12;j++)
-            cin>>EMPLEADOS[i].ven[j];
-        cin>>EMPLEADOS[i].sal;
-    }
-    return 0;
+int main() {
+	EMP EMPLEADOS[100];
+	int n;
+
+	cout << "Ingrese la cantidad de empleados: ";
+	cin >> n;
+	cout << "\n========= REGISTRO DE EMPLEADOS =========\n";
+	for (int i = 0; i < n; i++) {
+		cout << "Empleado #" << i + 1 << "\n";
+		cout << "Num empleado: ";
+		cin >> EMPLEADOS[i].num;
+
+		cin.ignore();
+		cout << "Nombre: ";
+		getline(cin, EMPLEADOS[i].nom);
+
+		for (int j = 0; j < 12; j++) {
+			cout << "  Venta " << j + 1 << ": ";
+			cin >> EMPLEADOS[i].ven[j];
+		}
+
+		cout << "Salario: ";
+		cin >> EMPLEADOS[i].sal;
+
+		cout << "------------------------------------------\n";
+	}
+
+	return 0;
 }
