a0043781b7132e6ebde08703075bcc494f505fff
japalominoc <japalominoc@unjbg.edu.pe>
2025-06-26 00:17:04 -0500

    Actualizando modificar contato

--- Contacto.cpp
+++ Contacto.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 struct correo{
-    string user;
-    string domain;
+	string user;
+	string domain;
 };
 
 struct contactoEmail{
-    string nom;
-    char sex;
-    int edad;
-    correo email;
+	string nom;
+	char sex;
+	int edad;
+	correo email;
 };
 
 void leerCorreo(correo &, string, string);
@@ -18,24 +18,24 @@ void leerContacto(contactoEmail &, string, char, int, correo);
 void imprimeContacto(contactoEmail &);
 
 int main(){
-    int n, op,modi;
-    string nom, user, domain;
-    char sex,op2;
-    int edad;
-    correo email;
-    contactoEmail cont, lista[100];
-    n = 0;
-    do{
-        system("cls");
-        cout<<"Menu de opciones -------------------------"<<endl;
-        cout<<"1. Agregar contacto"<<endl;
-        cout<<"2. Mostrar contactos"<<endl;
-        cout<<"3. Modificar contactos"<<endl;
-        cout<<"0. Salir"<<endl;
-        cout<<"Elige una opcion: "; cin>>op;
-        switch(op){
-            case 1:
-                cout<<"Ingrese los datos de un usuario: "<<endl;
+	int n,op;
+	string nom, user, domain;
+	char sex;
+	int edad;
+	correo email;
+	contactoEmail cont, lista[100];
+	n=0;
+	do{
+		system("cls");
+		cout<<"Menu de opciones --------------------"<<endl;
+		cout<<"1. Agregar contacto"<<endl;
+		cout<<"2. Mostrar contacto"<<endl;
+		cout<<"3. Modificar contacto"<<endl;
+		cout<<"0. Salir"<<endl;
+		cout<<"Elige una opcion: ";cin>>op;
+		switch(op){
+			case 1:
+				cout<<"Ingrese los datos de un usuario: "<<endl;
                 cin.ignore();
                 cout<<"Ingrese el nombre del contacto: "; getline(cin,nom);
                 cout<<"Ingrese el sexo (M/F): "; cin>>sex;
@@ -61,46 +61,54 @@ int main(){
                 system("pause");
                 break;
             case 3:
-                cout<<"Ingrese el contacto que desea modificar:";
-                cin>>modi;
-                cout<<"¿Desea modificar el nombre del contacto?(S/N): ";
-                cin>>op2;
-                    if(op2='S'){
-                        cout<<"Ingrese el nombre del contacto: "; getline(cin,nom);
-                }
-                cout<<"¿Desea modificar el sexo?(S/N): ";
-                cin>>op2;
-                    if(op2='S'){
-                        cout<<"Ingrese el sexo (M/F): "; cin>>sex;
-                    }
-                cout<<"¿Desea modificar la edad?(S/N): ";
-                cin>>op2;
-                if(op2='S'){
-                    cout<<"Ingrese la edad: "; cin>>edad;
-                }
-                cout<<"¿Desea modificar el correo?(S/N): ";
-                cin>>op2;
-                    if(op2='S'){
-                        cout<<"Ingrese el correo electronico (usuario@dominio): "<<endl;
-                    }
-                cout<<"¿Desea modificar la edad?(S/N): ";
-                cin>>op2;
-                    if(op2='S'){
-
-                    }
+            	if(n ==0){
+            			cout<<"No hay contacto para modificar"<<endl;
+				}
+				else{
+					int mod;
+            		cout<<"Ingrese el contacto que desea modificar"<<endl;
+            		cin>>mod;
+            		mod--;
+            		if(mod>=0 && mod<n){
+            			cin.ignore();
+            			cout<<"Ingrese el nombre del contacto: "; getline(cin,nom);
+              		  	cout<<"Ingrese el sexo (M/F): "; cin>>sex;
+	               		cout<<"Ingrese la edad: "; cin>>edad;
+	                	cout<<"Ingrese el correo electronico (usuario@dominio): "<<endl;
+                		cout<<"\tIngrese el usuario: "; cin>>user;
+                		cout<<"\tIngrese el dominio: "; cin>>domain;
+                		
+                		leerCorreo(email,user, domain);
+                		leerContacto(lista[mod],nom,sex,edad,email);
+                		
+                		cout<<"Modificacion exitosa"<<endl;
+					}
+					else{
+						cout<<"Indice fuera del rango"<<endl;
+					}
+					cout<<"No hay contacto para modificar"<<endl;
+				}
+				system("pause");
                 break;
             case 0:
-                cout<<"Esta seguro de salir? (S/N): ";
-                break;
+            	char salir;
+                cout << "¿Está seguro de salir? (S/N): ";
+				cin >> salir;
+				if (salir == 'S' || salir == 's') {
+					cout << "Saliendo del programa..." << endl;
+					break;
+				} else {
+					op = -1;
+				}
+				break;
             default:
                 cout<<"Opcion no valida!"<<endl;
                 system("pause");
                 break;
-        }
-    } while(op != 0);
-    return 0;
+		}
+	}while(op !=0);
+	return 0;
 }
-
 void leerContacto(contactoEmail &c, string n, char s, int e, correo em){
     c.nom = n;
     c.sex = s;
